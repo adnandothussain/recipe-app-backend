a383866a33d08e0f7e3945a15b9697f555946f6f
Adnan Hussain <adnanrajput42@gmail.com>
2021-07-17 23:39:10 +0500

    chore: update schema

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,17 +1,19 @@
 import { schemaComposer } from 'graphql-compose';
-import { RecipeTC, UserTC, searchRecipes, searchRestraunts } from './composers';
+import { RecipeTC, UserTC, searchRecipes, searchRestraunts, BookmarkTC } from './composers';
 import { RecipeRequestTC, listRecipeRequests } from './composers/RecipeRequestTC';
 
 schemaComposer.Query.addFields({
   recipes: RecipeTC.getResolver('recipeFeed'),
+  userById: UserTC.mongooseResolvers.findById(),
   searchRecipes,
   searchRestraunts,
-  userById: UserTC.mongooseResolvers.findById(),
   listRecipeRequests,
 });
 
 schemaComposer.Mutation.addFields({
   requestRecipe: RecipeRequestTC.getResolver('requestRecipe'),
+  createRecipe: RecipeTC.getResolver('createRecipe'),
+  createBookmark: BookmarkTC.getResolver('createBookmark'),
 });
 
 export default schemaComposer.buildSchema();
