4ad1e93e167f38cd88e82a03566b450a7790bb40
Adnan Hussain <adnanrajput42@gmail.com>
2021-08-05 23:37:43 +0500

    chore: add compose relay to enable relay paging with graphql node, interface

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,6 +1,37 @@
 import { schemaComposer } from 'graphql-compose';
+import composeWithRelay, {
+  TypeMapForRelayNode,
+  getNodeInterface,
+  toGlobalId,
+  // @ts-ignore
+} from 'graphql-compose-relay';
+
 import { RecipeTC, UserTC, searchRecipes, searchRestraunts, BookmarkTC } from './composers';
 import { RecipeRequestTC, listRecipeRequests } from './composers/RecipeRequestTC';
+
+composeWithRelay(schemaComposer.Query);
+[UserTC].forEach((tc) => {
+  tc.addInterface(getNodeInterface(schemaComposer));
+  /**
+   * The following two steps are necessary for relay to be able to resolve
+   * any type that implements Node interface. Without these two steps we are not
+   * able to query for a record using the global ID. This is also problematic
+   * when using refetchable features of relay that rely on global ID
+   */
+  const findById = tc.mongooseResolvers.findById();
+  TypeMapForRelayNode[tc.getTypeName()] = {
+    resolver: findById,
+    tc,
+  };
+  tc.addFields({
+    id: {
+      type: 'ID!',
+      description: 'The globally unique ID among all types',
+      resolve: (source: any) => toGlobalId(tc.getTypeName(), tc.getRecordId(source)),
+    },
+  });
+  tc.setRecordIdFn((source) => source._id);
+});
 
 schemaComposer.Query.addFields({
   recipes: RecipeTC.getResolver('recipeFeed'),
